bb716271dc814b34e52893e979b09a3a410b6757
Anastasia-Kiseleva12 <pm15.kiseleva1@gmail.com>
2024-12-08 22:52:58 +0300

    music top

--- OurGameAvaloniaApp/Views/MainWindow.axaml.cs
+++ OurGameAvaloniaApp/Views/MainWindow.axaml.cs
@@ -1,11 +1,54 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using LibVLCSharp;
+using LibVLCSharp.Shared;
+using System;
+using System.Diagnostics;
 
-namespace OurGameAvaloniaApp.Views;
-
-public partial class MainWindow : Window
+namespace OurGameAvaloniaApp.Views
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private MediaPlayer _mediaPlayer;
+        private LibVLC _libVLC;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Core.Initialize();
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.IsVisible = false; // Скрыть меню
+            try
+            {
+                var filePath = @"C:\Users\Ася\Desktop\Наша игра\Our-game\OurGameAvaloniaApp\Assets\music.mp3";
+                if (!System.IO.File.Exists(filePath))
+                {
+                    Debug.WriteLine("Файл не найден!");
+                    return;
+                }
+
+                var media = new Media(_libVLC, filePath, FromType.FromPath);
+                _mediaPlayer.Play(media);
+                _mediaPlayer.Volume = 70; // Установите громкость на 100%
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика для открытия настроек
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // Закрытие приложения
+        }
     }
 }
